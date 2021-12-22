31005953f0226292a70adb1f7cfebc48dadc77e0
Jerry de Vos <devos.winsum@gmail.com>
2021-12-22 20:49:00 +0100

    test commit for automation

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Plastic Scanner documentation',
-  tagline: 'Plastic Scanner documentation',
+  tagline: 'Documentation for the Plastic Scanner development',
   url: 'https://Plastic-Scanner.github.io',
   baseUrl: '/documentation/',
   onBrokenLinks: 'throw',
